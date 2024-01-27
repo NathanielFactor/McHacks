a38dce243aa5368ae1fd70d116e3b6028a1826e4
Nathaniel Factor <bbnat123@outlook.com>
2024-01-27 16:17:44 -0500

    finished db

--- nathaniel/init_db.py
+++ nathaniel/init_db.py
@@ -1,16 +1,13 @@
 import sqlite3
 
-connection = sqlite3.connect('Conversations.db')
+connection = sqlite3.connect('database.db')
+
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
 
 cur = connection.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS Conversations(\
-    Userid INTEGER PRIMARY KEY AUTOINCREMENT,\
-    UserName TEXT NOT NULL,\
-    Created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,\
-    Messages TEXT NOT NULL)\
-    ")
 
 connection.commit()
 connection.close()
-    

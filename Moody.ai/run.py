12a09994e6c64945d00572b18471d299c7c51078
Nathaniel Factor <bbnat123@outlook.com>
2024-01-28 04:54:44 -0500

    updated bot2 route

--- Moody.ai/run.py
+++ Moody.ai/run.py
@@ -6,6 +6,9 @@ import time
 import sklearn
 import numpy as np
 import pickle
+import jsonify
+import bot2
+
 
 app = Flask(__name__)
 app.secret_key = "super-secret-key"
@@ -197,6 +200,14 @@ def create_spotify_oauth():
         redirect_uri= 'http://127.0.0.1:5000/callback',
         scope="user-library-read playlist-modify-public playlist-modify-private user-read-recently-played user-top-read"
     )
+    
+@app.route('/api/generate-quote', methods=['POST'])
+def generate_quote(mood):
+
+    # Use your OpenAI integration to generate a response based on the prompt
+    generated_response = bot2.ai_response2(mood)
+
+    return jsonify({'quote': generated_response})
 
 
 AUTH_URL = "https://accounts.spotify.com/authorize"
